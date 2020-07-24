682b2949bb8e2de1747d808047e0470a39b964e4
unknown <Reyli Rodriguez>
2020-07-24 09:13:52 -0500

    Updates

---
+++ precache-manifest.e156b7cd07302a02432e8ff30faf9a5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ef5024f0e131e5622f5bd2c4028f69",
+    "url": "/my-app2react/index.html"
+  },
+  {
+    "revision": "88632a211224b44f466e",
+    "url": "/my-app2react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "310653cd83a3266a5588",
+    "url": "/my-app2react/static/js/2.3e33d232.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app2react/static/js/2.3e33d232.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88632a211224b44f466e",
+    "url": "/my-app2react/static/js/main.238aa776.chunk.js"
+  },
+  {
+    "revision": "2ed1fdef4a83d822977c",
+    "url": "/my-app2react/static/js/runtime-main.c8649439.js"
+  }
+]);
